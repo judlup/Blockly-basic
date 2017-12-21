d1e0315e4d960d48fe8cd96be5ec4fb69b3efa23
judlup <judlup@gmail.com>
2017-12-20 20:41:13 -0500

    Blockly-basic

---
+++ js/custom_blocks.js
@@ -0,0 +1,19 @@
+Blockly.Blocks['string_length'] = {
+  init: function() {
+    this.appendValueInput('VALUE')
+        .setCheck('String')
+        .appendField('length of');
+    this.setOutput(true, 'Number');
+    this.setColour(160);
+    this.setTooltip('Returns number of letters in the provided text.');
+    this.setHelpUrl('http://www.w3schools.com/jsref/jsref_length_string.asp');
+  }
+};
+
+Blockly.JavaScript['string_length'] = function(block) {
+  // String or array length.
+  var argument0 = Blockly.JavaScript.valueToCode(block, 'VALUE',
+      Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+  console.log("ARG",argument0);
+  return [argument0 + '.length', Blockly.JavaScript.ORDER_MEMBER];
+};
--- js/es.js
+++ js/es.js
@@ -5,7 +5,30 @@
 goog.provide('Blockly.Msg.es');
 
 goog.require('Blockly.Msg');
-
+var MSG = {
+	title:"Blockly Basic", 
+	blocks: "Bloques",
+  linkTooltip: "Guarda conexión a los bloques.",
+  runTooltip: "Ejecute el programa definido por los bloques en el área de trabajo.",
+  badCode: "Error del programa:\n%1",
+  timeout: "Se excedio el máximo de iteraciones ejecutadas permitidas.",
+  trashTooltip: "Descartar todos los bloques.",
+  catLogic: "Lógica",
+  catLoops: "Secuencias",
+  catMath: "Matemáticas",
+  catText: "Texto",
+  catLists: "Listas",
+  catColour: "Color",
+  catVariables: "Variables",
+  catFunctions: "Funciones",
+  listVariable: "lista",
+  textVariable: "texto",
+  httpRequestError: "Hubo un problema con la petición.",
+  linkAlert: "Comparte tus bloques con este enlace:\n\n%1",
+  hashError: "«%1» no corresponde con ningún programa guardado.",
+  xmlError: "No se pudo cargar el archivo guardado.  ¿Quizá fue creado con otra versión de Blockly?",
+  badXml: "Error de análisis XML:\n%1\n\nSelecciona OK para abandonar tus cambios o Cancelar para seguir editando el XML."
+};
 /** @export */ Blockly.Msg.ADD_COMMENT = "Añadir comentario";
 /** @export */ Blockly.Msg.CANNOT_DELETE_VARIABLE_PROCEDURE = "No se puede eliminar la variable \"%1\" porque es parte de la definición de la función \"%2\"";
 /** @export */ Blockly.Msg.CHANGE_VALUE_TITLE = "Cambiar el valor:";
@@ -423,3 +446,4 @@ goog.require('Blockly.Msg');
 /** @export */ Blockly.Msg.TEXTS_HUE = "160";
 /** @export */ Blockly.Msg.PROCEDURES_HUE = "290";
 /** @export */ Blockly.Msg.COLOUR_HUE = "20";
+
